04fc549011bdccf2211f608a509127d0369d00f0
Pedro Costa <antonhyonhi@gmail.com>
2019-11-26 05:41:50 -0300

    mensagem

--- gerador_slr.py
+++ gerador_slr.py
@@ -1,7 +1,17 @@
 from bs4 import BeautifulSoup #Precisa instalar com $ pip3 install bs4
+from csv import writer
 
 doc = open('SLR Parser Generator.html', 'r').read()
 
 parsed_doc = BeautifulSoup(doc, 'html.parser')
 
-print(doc)
+tabela = parsed_doc.find('div', id = 'lrTableView').table
+cabecalho = tabela.thead.contents[2].contents
+cabecalho.insert(0, tabela.thead.contents[1].th)
+linhas = tabela.tbody.contents
+
+for celula in cabecalho:
+    print (celula)
+
+for ceula in linhas:
+    print (celula)
